5ddf2fac5233c309306e8d5f4cafe10c703a6f2d
Gabriel Dias <93868997+gabrielcarvalhodias@users.noreply.github.com>
2023-12-08 12:38:26 -0300

    ??

---
+++ src/context/Provider.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { AppContext } from './AppContext';
+
+function Provider({ children }) {
+    const [info, setInfo] = useState([]);
+    const [cartItems,setCartItems] = useState([]);
+    const [isCartVisible, setIsCartVisible] = useState(false);
+
+    const contextValue = {
+       info,
+       
+       setInfo,
+       cartItems,
+       setCartItems,
+       isCartVisible, 
+       setIsCartVisible,
+    };
+
+    return (
+        <AppContext.Provider value={contextValue}>
+            {children}
+        </AppContext.Provider>
+    );
+}
+
+Provider.propTypes = {
+    children: PropTypes.any.isRequired,
+};
+
+export default Provider;
